c4f57e14080743521ed69f63ae63deb9a582bcae
Josue Hernandez <josuehdz0@gmail.com>
2023-05-26 12:32:52 -0600

    Home components setup

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -3,6 +3,19 @@ import './home.css';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>Home
+      {/* these will be components */}
+      <br></br>
+      - Who We Are
+      <br></br>
+      - Core Values
+      <br></br>
+      - Service Information
+      <br></br>
+      - Events Calendar
+
+
+    </div>
+
   )
 }
