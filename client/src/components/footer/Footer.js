04d38beaee1ab4719a5612e2e882cd0920d19111
David Pascual <davidpascual200@gmail.com>
2023-05-26 11:40:15 -0700

    styled footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import './footer.css'
+import './footer.css';
+import triumph from '../../assets/logo.001.beige.PNG';
+
+const churchName = "Triumph Seventh-Day Adventist Chruch";
+const address = "410 Butte Ave S";
+const cityStateZip = "Auburn WA 98002";
+const phoneNumber = "253-951-0250";
+
+//================================
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <div className='footer container-sm d-flex flex-md-row flex-column'>
+        <div className='logoContainer2'>
+            <img className='triumphLogo2' src={triumph} alt="" />
+
+        </div>
+        <div className='footerDesc'>
+            <p>{churchName}</p>
+            <p>{address}</p>
+            <p>{cityStateZip}</p>
+            <p>{phoneNumber}</p>
+        </div>
+        <div className="created">
+            <p>Copyright Triumph Adventist Chruch &copy; 2023</p>
+        </div>
+    </div>
+    </>
   )
 }
