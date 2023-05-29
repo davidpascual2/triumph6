a2d99bcdb6c1eec4a50361a8b51f21c0bb7d8802
Josue Hernandez <josuehdz0@gmail.com>
2023-05-29 17:43:32 -0600

    Nav bar adjustments

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -7,40 +7,40 @@ export default function Navbar() {
   return (
     <>
       <div className="nav container-fluid">
-        
+
         <header className="header1">
-          <img className="triumphLogo" src={triumph} alt='tri'/>
+          <img className="triumphLogo" src={triumph} alt='tri' />
         </header>
 
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                {/* <a class="navbar-brand" href="#">Triumph</a> */}
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navcollapse">
+          <div class="container-fluid">
+            {/* <a class="navbar-brand" href="#">Triumph</a> */}
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li className="nav-item">
-                            <Link to='/' className="nav-link active" aria-current="page" href="#">Home</Link>
-                        </li>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
 
-                        <li className="nav-item"> 
-                            <Link to='/leaders' className="nav-link" >Leaders</Link>
-                        </li>
-            
-                        <li className="nav-item"> 
-                            <Link to='/contactUs' className="nav-link" >Contact Us</Link>
-                        </li>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li className="nav-item">
+                  <Link to='/' className="nav-link active" aria-current="page" href="#">Home</Link>
+                </li>
 
-                        <li className="nav-item">
-                            <Link to='/give' className="nav-link" >Give</Link>
-                        </li>
-                    </ul>
-                </div>
+                <li className="nav-item">
+                  <Link to='/leaders' className="nav-link" >Leaders</Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to='/contactUs' className="nav-link" >Contact Us</Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to='/give' className="nav-link" >Give</Link>
+                </li>
+              </ul>
             </div>
-          </nav>
+          </div>
+        </nav>
       </div>
     </>
   )
