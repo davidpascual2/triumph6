95f578124c29c373cffb73cb5966fbc9b5f7858d
David Pascual <davidpascual200@gmail.com>
2023-05-30 10:31:22 -0700

    fixed whoweare

--- client/src/components/whoWeAre/WhoWeAre.js
+++ client/src/components/whoWeAre/WhoWeAre.js
@@ -3,37 +3,31 @@ import './whoWeAre.css'
 import { useEffect } from "react";
 
 
-
-
-
 export default function WhoWeAre() {
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.pageYOffset;
-      const parallaxContainer = document.querySelector('.whoWeAreComp');
-      const parallaxText = document.querySelector('.whoWeAreText');
 
-      parallaxContainer.style.backgroundPositionY = `${-scrollPosition * 0.03}px`;
-      parallaxText.style.transform = `translateY(${scrollPosition * 0.2}px)`;
-    };
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollPosition = window.pageYOffset;
+  //     const parallaxContainer = document.querySelector('.whoWeAreComp');
+  //     const parallaxText = document.querySelector('.whoWeAreText');
 
-    window.addEventListener('scroll', handleScroll);
+  //     parallaxContainer.style.backgroundPositionY = `${-scrollPosition * 0.03}px`;
+  //     parallaxText.style.transform = `translateY(${scrollPosition * 0.2}px)`;
+  //   };
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  //   window.addEventListener('scroll', handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, []);
 
   return (
     <>
       <div className='whoWeAreComp'>
-      <div className='container-fluid d-flex whoWeAreContainer'>
-          <div className='row justify-content-start mb-md-5 ms-md-5 mb-3 mx-1'>
-            <div className="whoWeAreText col-md-6">
+        <div className='whoWeAreText'>
               <h1 className="">Who We Are</h1>
               <p>All are welcome. Whether you're seeking answers to life's questions, searching for a spiritual home, or simply looking for a place to belong, we invite you to join us at [Church Name]. Together, let's discover the joy of walking in faith, finding strength in fellowship, and experiencing the transformative power of God's love.</p>
-            </div>
-          </div>
         </div>
       </div>
     </>
