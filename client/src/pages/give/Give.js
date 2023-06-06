9e24302441381b5895b3754c75ebaacec16e26d7
Josue Hernandez <josuehdz0@gmail.com>
2023-06-05 23:19:46 -0600

    Dumb message

--- client/src/pages/give/Give.js
+++ client/src/pages/give/Give.js
@@ -5,16 +5,17 @@ import Navbar from '../../components/navbar/Navbar';
 export default function Give() {
   return (
     <>
-    <div>
-      {/* <Navbar /> */}
-      <a 
-        type="submit" 
-        className="giveButton"
-        href='https://adventistgiving.org/donate/ANIM2I'
-      >
-      Give Now
-      </a>
-    </div>
+      <div>
+        Hello from Give <br></br>
+        Add a Bible verse here
+        <a
+          type="submit"
+          className="giveButton"
+          href='https://adventistgiving.org/donate/ANIM2I'
+        >
+          Give Now
+        </a>
+      </div>
     </>
   )
 }
