9f08961dc4c7e8990a66a00a06d7f68e78f79a3c
David Pascual <davidpascual200@gmail.com>
2023-06-14 10:08:52 -0700

    added "??"

--- client/src/components/whoWeAre/WhoWeAre.js
+++ client/src/components/whoWeAre/WhoWeAre.js
@@ -29,7 +29,7 @@ export default function WhoWeAre() {
         <div className='d-flex whoWeAreContainer'>
           <div className='row justify-content-md-start justify-content-center mb-md-5 ms-md-5 mb-3 mx-1'>
             <div className="whoWeAreText col-md-7 col-11">
-              <h1 className="">Who We Are!</h1>
+              <h1 className="">Who We Are??</h1>
               <p>All are welcome. Whether you're seeking answers to life's questions, searching for a spiritual home, or simply looking for a place to belong, we invite you to join us at [Church Name]. Together, let's discover the joy of walking in faith, finding strength in fellowship, and experiencing the transformative power of God's love.</p>
             </div>
           </div>
