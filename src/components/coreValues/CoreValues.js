d78bf9bec4dd2a5c606d2d4f944f7e8b8344c7d3
Josue Hernandez <josuehdz0@gmail.com>
2023-06-16 15:10:10 -0600

    core values fix

--- src/components/coreValues/CoreValues.js
+++ src/components/coreValues/CoreValues.js
@@ -10,18 +10,18 @@ export default function CoreValues() {
                 </div>
 
                 <div className="valuesCont">
-                    <div className="row">
-                        <div className='value col-md-6 col-12'>
+                    <div className="row justify-content-center">
+                        <div className='value col-md-7 col-12'>
                             <h4>Follow Jesus</h4>
                             <p>It’s all about Jesus. We believe he loves, he cares, and we are better when we follow him.</p>
                         </div>
 
-                        <div className='value col-md-6 col-12'>
+                        <div className='value col-md-7 col-12'>
                             <h4>Love others </h4>
                             <p>We are not a social club. We don’t care just for our insiders. We actively seek to love people outside of our community.</p>
                         </div>
 
-                        <div className='value col-md-6 col-12'>
+                        <div className='value col-md-7 col-12'>
                             <h4>Extend the Kingdom</h4>
                             <p>We are on a mission to make the world a better place. We firmly believe that our best bet to achieve that is by spreading the good news of Jesus. </p>
                         </div>
